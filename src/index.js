7f80d2e6bf60f511655089db833a74368ddf5214
WuGe01 <bc781208@hotmail.com>
2021-11-08 10:35:57 +0800

    增加新增成功提示

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Home from "./pages/Home";
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Home></Home>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,32 @@
 import { useState,useEffect } from "react";
+import { API_GET_DATA } from "../global/constants";
 
 import Edit from "./components/Edit";
 import List from "./components/List";
 import "./index.css";
 
+async function callAPI(setData) {
+  const res = await fetch(API_GET_DATA);
+  const { data } = await res.json();
+  console.log(data);
+  setData(data);
+  // return data;
+};
+
 const Home = () => {
+
   const [data, setData] = useState([]);
 
   useEffect(()=>{
+
+    // callAPI(setData);
+
     return() => {
       window.alert("新增成功");
     }
-  },[data])
+  },[data]);
+
+ 
 
   return (
     <div className="app">
